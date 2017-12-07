14a6244d8d65b4308f3f95a0812f98d691e89410
Dimama <citrus_m@mail.ru>
2017-12-07 22:32:56 +0300

    [add] Huffman compression done

--- Information Security/Compression/huffman.py
+++ Information Security/Compression/huffman.py
@@ -1,12 +1,12 @@
 from tree import Node
-
+import struct
+import sys
 
 class Huffman():
     
     def __init__(self, filename):
         self.table = self.make_frequency_table(filename)
-        self.tree = self.make_tree()
-        #self.code_table = self.make_code_table()
+        self.codes_table = self.make_codes_table()
 
     def make_frequency_table(self, filename):
         """
@@ -24,25 +24,27 @@ class Huffman():
                         table[item] += 1
         return table                    
 
-    def make_tree(self):
+    def make_codes_table(self):
         """
-        Построение дерева кодирования
+        Построение таблицы кодов
         """
         # создаем список свободных узлов
         nodes = []
         for i in range(len(self.table)):
-            nodes.append(Node(i, self.table[i]))
+            if self.table[i] > 0:
+                nodes.append(Node(i, self.table[i]))
 
-        nodes.sort(key=Node.sort_by_freq)
-
-        #for node in nodes:
-        #    print(node)
         leafs = [] # Список узлов нижнего уровня
-
+        codes = [] # Таблица кодов
+    
         # создаем дерево: создаем один узел, объединяя два узла с наименьшими весами, записываем в список
-        count = 0
+        
+        if len(nodes) == 1:
+            leafs.append(nodes[0])
+            codes.append((nodes[0].value, "0"))
+            return codes
+
         while len(nodes) > 1:
-            count += 1
             nodes.sort(key=Node.sort_by_freq) # сортируем узлы по весам
             left, right = nodes.pop(0), nodes.pop(0)   # выбираем два узла с наименьшими весами
             new_node = Node(None, left.freq + right.freq, None, left, right) # создаем новый узел
@@ -58,20 +60,12 @@ class Huffman():
             
             nodes.append(new_node) # добавляем новый узел в список
         
-        #print("-------------")
-        #print(len(nodes))
-        #for node in nodes:
-            #print(node)
-
-        root = nodes[0] # корень дерева Хаффмана
-
         # создаем таблицу кодов
-        codes = []
-    
-
-        for i in range(len(leafs)):
+        # добавляем только коды символов, содержащихся в сжимаемом файле
+        for leaf in leafs:
             code = ""
-            node =  leafs[i]
+            node =  leaf
+            # проход по дереву до корня
             while True:
                 parent = node.parent
                 if parent == None:
@@ -81,23 +75,100 @@ class Huffman():
                 if node == parent.right:
                     code += "1"
                 node = parent
-            codes.append(code[::-1])
-
-        for i in range(len(codes)):
-            print("{}: {}".format(i, codes[i]))
+            codes.append((leaf.value, code[::-1])) # добавляем в таблицу кортеж (символ, код)
             
-        return nodes[0]
+        return codes
 
+    def bit_str_to_byte(self, s):
+        return struct.pack('B', int(s, 2)) 
 
-    def make_code_table():
+    def find_code(self, num):
+        for i in range(len(self.codes_table)):
+            if self.codes_table[i][0] == num:
+                return self.codes_table[i][1]
+
+        raise "Incorrect code table!"
+
+    def find_byte(self, code):
+        for i in range(len(self.codes_table)):
+            if self.codes_table[i][1] == code:
+                return self.codes_table[i][0]
+
+        return None    
+
+    def compress(self, filename):
         """
-        Создание таблицы кодов Хаффмана
+        Cжатия файла filname
+        return:
+        имя файла с таблицей кодов
+        имя сжатого файла
+        кол-во дописанных в байт нулей
         """
-        pass
-        
-        
-    def compress():
+        codes_filename = "codes.txt"
+
+        # создание файла с кодами
+        with open(codes_filename, "w") as codes_f:
+            for item in self.codes_table:
+                codes_f.write("{} {}\n".format(item[0], item[1]))
+
+        # сжатие
+        zeroes = 0
+        res_filename = "compressed_" + filename
+        with open(filename, "rb")  as f, open(res_filename, "wb") as res_f:
+            code_str = ""
+            while True:
+                s = f.read(1024)
+                if not len(s):
+                    break
+                else:
+                    for item in s:
+                        code = self.find_code(item)
+                        code_str += code
+                        if len(code_str) >= 8:
+                            byte_str = code_str[:8]
+                            code_str = code_str[8:]
+                            byte = self.bit_str_to_byte(byte_str)
+                            res_f.write(byte)
+
+            # дописать биты, если размер кода не кратен 8  
+            if len(code_str) > 0:
+                zeroes = 8 - len(code_str)
+                for i in range(zeroes):
+                    code_str += '0'
+                byte = self.bit_str_to_byte(code_str)
+                res_f.write(byte)
+
+        return codes_filename, res_filename, zeroes
+
+    def split_on_bytes(self, code_str, zeroes, file):
         pass
 
-    def decompress():
-        pass
+    def decompress(self, filename, compressed_filename, zeroes):
+        """
+        Восстановление исходного файла из сжатого
+        """
+        res_filename = "decompressed_" + filename
+        with open(compressed_filename, "rb") as f, open(res_filename, "wb") as res_f:
+            code_str = ""
+            while True:
+                s = f.read(1024)
+                if not len(s):
+                    break
+                else:
+                    for byte in s:
+                        code = bin(byte)[2:].zfill(8)
+                        code_str += code
+
+            if zeroes:
+                code_str = code_str[:-zeroes]
+
+            code = ""
+            while len(code_str):
+                code += code_str[0]
+                code_str = code_str[1:]
+                byte = self.find_byte(code)
+                if byte != None:
+                    res_f.write(struct.pack('B', byte))
+                    code = ""
+      
+        return res_filename
--- Information Security/Compression/main.py
+++ Information Security/Compression/main.py
@@ -1,10 +1,29 @@
+import sys
+import os
 from huffman import Huffman
 
 
 def main():
-    filename = "1.txt"
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        print("Задайте файл с помощью аргументов командной строки")
+        return
+
+    if not os.path.exists(filename):
+        print("Файл не найден!")
+        return
+    
+    print("Исходный файл: '{}' ({} байт)".format(filename, os.path.getsize(filename)))
     
     huf = Huffman(filename)
+    codes_filename, res_filename, zeroes = huf.compress(filename)
+    print("Сжатый файл: '{}'  ({} байт)".format(res_filename,  os.path.getsize(res_filename)))
+    print("Размер таблицы кодов: {} байт".format(sys.getsizeof(huf.codes_table)))
+    print("Нулей дописано в последний байт:", zeroes)
+
+    dec_filename = huf.decompress(filename, res_filename, zeroes)
+    print("Восстановленный файл: '{}'  ({} байт)".format(dec_filename,  os.path.getsize(dec_filename)))
 
 if __name__ == '__main__':
     main()
